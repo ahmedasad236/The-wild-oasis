3a362af8695198cd4d2982eb3bc76c99f3dbc99f
ahmedasad236 <ahmedasad236@gmail.com>
2023-08-14 18:16:49 +0300

    Create a new Heading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { styled } from 'styled-components';
 import GlobalStyles from './styles/globalStyles';
 import Input from './ui/Input';
 import Button from './ui/Button';
+import Heading from './ui/Heading';
 function App() {
   return (
     <>
       <GlobalStyles />
       <div>
+        <Heading as="h1">Heading 1</Heading>
+        <Heading as="h2">Heading 2</Heading>
+
+        <Heading as="h3">Heading 3</Heading>
+
         <Button>Check-in</Button>
         <Input
           placeholder="number of guests"
