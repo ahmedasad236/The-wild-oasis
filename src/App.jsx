5806f799aaa7b4a8d8bfbde82622d01fcb79fa92
ahmedasad236 <ahmedasad236@gmail.com>
2023-09-12 16:20:46 +0300

    Fetch Cabin Data

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,21 @@ import Bookings from './pages/Bookings';
 import Settings from './pages/Settings';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 6 * 1000
+    }
+  }
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -62,7 +73,7 @@ function App() {
           />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
