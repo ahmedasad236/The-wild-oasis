a374ece43dfb37f4b223f7660b2167e71cb6f942
ahmedasad236 <ahmedasad236@gmail.com>
2023-09-12 15:17:32 +0300

    Try the connection with supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,10 @@
 import supabase from './supabase';
 
 export async function getCabins() {
-  let { data, error } = await supabase.from('Cabins').select('*');
-
+  let { data: Cabins, error } = await supabase.from('Cabins').select('*');
   if (error) {
     throw new Error('Cabins could not be loaded');
   }
 
-  return data;
+  return Cabins;
 }
