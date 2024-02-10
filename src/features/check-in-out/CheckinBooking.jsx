c9b6d317758f6941d56a7e7c11638c26e2d3f4be
ahmedasad236 <ahmedasad236@gmail.com>
2024-02-10 13:37:07 +0200

    Add the option of checking in whith breakfast

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -15,6 +15,7 @@ import { useEffect, useState } from 'react';
 
 import { formatCurrency } from '../../utils/helpers';
 import { useCheckin } from './hooks/useCheckin';
+import { useSettings } from '../settings/hooks/useSettings';
 
 const Box = styled.div`
   /* Box */
@@ -27,19 +28,27 @@ const Box = styled.div`
 function CheckinBooking() {
   const moveBack = useMoveBack();
   const [confirmCheckin, setConfirmCheckin] = useState(false);
+  const [addBreakfast, setAddBreakfast] = useState(false);
+
   const { booking, isLoading } = useBooking();
   const { checkin, isCheckingIn } = useCheckin();
+  const { settings, isLoading: isLoadingSettings } = useSettings();
 
   useEffect(() => {
     if (booking?.is_paid) {
       setConfirmCheckin(true);
     }
+
+    if (booking?.has_breakfast) {
+      setAddBreakfast(true);
+    }
   }, [booking]);
 
-  if (isLoading) {
+  if (isLoading || isLoadingSettings || isCheckingIn) {
     return <Spinner />;
   }
 
+  const { breakfast_price } = settings;
   const {
     id: bookingId,
     Guests,
@@ -49,9 +58,20 @@ function CheckinBooking() {
     num_nights
   } = booking;
 
+  const optionalBreakfastPrice = breakfast_price * num_guests * num_nights;
   function handleCheckin() {
-    if (confirmCheckin) {
-      checkin();
+    if (!confirmCheckin) return;
+    if (addBreakfast) {
+      checkin({
+        booking_id: bookingId,
+        breakfast: {
+          has_breakfast: true,
+          extras_price: optionalBreakfastPrice,
+          total_price: optionalBreakfastPrice + total_price
+        }
+      });
+    } else {
+      checkin({ booking_id: bookingId });
     }
   }
 
@@ -63,6 +83,21 @@ function CheckinBooking() {
       </Row>
 
       <BookingDataBox booking={booking} />
+      {!has_breakfast && (
+        <Box>
+          <CheckBox
+            checked={addBreakfast}
+            onChange={() => {
+              setAddBreakfast((prevAddBreakfast) => !prevAddBreakfast);
+              setConfirmCheckin(false);
+            }}
+            id="add-breakfast"
+          >
+            Want to add breakfast for {optionalBreakfastPrice}
+            $?
+          </CheckBox>
+        </Box>
+      )}
 
       <Box>
         <CheckBox
@@ -74,8 +109,14 @@ function CheckinBooking() {
           }
         >
           I confirm that {Guests.full_name} has paid the total amount of{' '}
-          {formatCurrency(total_price)} for {num_guests} guests, for{' '}
-          {num_nights}
+          {!addBreakfast
+            ? formatCurrency(total_price)
+            : `${formatCurrency(
+                total_price + optionalBreakfastPrice
+              )} (${formatCurrency(total_price)} + ${formatCurrency(
+                optionalBreakfastPrice
+              )})`}
+          for {num_guests} guests, for {num_nights}
           nights, and that breakfast is{' '}
           {has_breakfast ? 'included' : 'not included'}.
         </CheckBox>
--- src/features/check-in-out/hooks/useCheckin.js
+++ src/features/check-in-out/hooks/useCheckin.js
@@ -1,16 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { updateBooking } from '../../../services/apiBookings';
 import toast from 'react-hot-toast';
 
 export const useCheckin = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const { booking_id } = useParams();
   const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
-    queryKey: ['checkin', booking_id],
-    mutationFn: () =>
-      updateBooking(booking_id, { is_paid: true, status: 'checked-in' }),
+    mutationFn: ({ booking_id, breakfast }) =>
+      updateBooking(booking_id, {
+        is_paid: true,
+        status: 'checked-in',
+        ...breakfast
+      }),
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} has been checked in successfully`);
       queryClient.invalidateQueries({ active: true });
